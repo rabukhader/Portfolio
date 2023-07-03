a44820317c79465848ecc96482622c4ffb47b7d1
Rasheed Abu Khader <103368328+rabukhader@users.noreply.github.com>
2023-07-03 20:40:47 +0300

    deploy fix2

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import {AiOutlineUser, AiOutlineHome} from 'react-icons/ai'
 import {BiBook , BiMessageSquareAdd} from 'react-icons/bi'
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './Portfolio.css'
 import p1 from '../../assets/p1.jpg'
